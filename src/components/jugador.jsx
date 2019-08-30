c4815d149daa8d37f261d2c0e827d525387d0119
luciomurature <luciomurature@gmail.com>
2019-08-30 11:35:42 -0300

    fetching random names from api to identify different jugadores

--- src/components/jugador.jsx
+++ src/components/jugador.jsx
@@ -15,7 +15,9 @@ class Jugador extends Component {
 
   handleChange = e => {
     console.log(e.target.value);
-    if (isNaN(e.target.value)) { return;}
+    if (isNaN(e.target.value)) {
+      return;
+    }
     this.setState({
       puntosAgregar: parseInt(e.target.value)
     });
@@ -23,33 +25,37 @@ class Jugador extends Component {
 
   handleLimit = () => {
     if (parseInt(this.state.puntos) > parseInt(this.state.limite)) {
-      alert("Jugador " + (this.props.id + 1) + " perdio.")
+      alert("Jugador " + (this.props.id + 1) + " perdio.");
       this.props.onDelete(this.props.id);
-    } 
-  }
+    }
+  };
 
-  handlePuntos = (e) => {
+  handlePuntos = e => {
     e.preventDefault();
-    this.setState({ puntos: parseInt(this.state.puntos) + parseInt(this.state.puntosAgregar) });
-  }
+    this.setState({
+      puntos: parseInt(this.state.puntos) + parseInt(this.state.puntosAgregar)
+    });
+  };
 
-    
+  componentDidMount() {
+    fetch("https://randomuser.me/api/")
+      .then(response => response.json())
+      .then(data =>
+        this.setState({ nombre: data.results[0].name.first[0].toUpperCase() + data.results[0].name.first.slice(1) + " " + data.results[0].name.last[0].toUpperCase() + data.results[0].name.last.slice(1) }));
+      }
+  
   render() {
-
     this.handleLimit();
     return (
-      <div className="container" style={{textAlign: 'center'}}>
+      <div className="container" style={{ textAlign: "center" }}>
         <h3>
           <span className="badge badge badge-dark m-3">
-            {this.state.nombre} {this.props.id + 1 }
+            {this.state.nombre}
           </span>
         </h3>
-        <span
-          style={{ fontSize: 20 }}
-          className="badge m-1 badge badge-dark "
-        >
+        <span style={{ fontSize: 20 }} className="badge m-1 badge badge-dark ">
           {this.state.puntos} Puntos
-            </span>
+        </span>
 
         <form>
           <label>
@@ -61,15 +67,22 @@ class Jugador extends Component {
             ></input>
           </label>
           <label>
-<button onClick={this.handlePuntos} className="btn btn-dark m-2">+</button>
-
+            <button onClick={this.handlePuntos} className="btn btn-dark m-2">
+              +
+            </button>
           </label>
         </form>
 
         <div>
           <button
             className="btn btn-danger btn-bg"
-            onClick={() => { if (window.confirm('¿Estas seguro que deseas eliminar el jugador?')) { this.props.onDelete(this.props.id) } }}
+            onClick={() => {
+              if (
+                window.confirm("¿Estas seguro que deseas eliminar el jugador?")
+              ) {
+                this.props.onDelete(this.props.id);
+              }
+            }}
           >
             Eliminar
           </button>
