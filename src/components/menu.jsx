59bae43f11cab1e6766014d35a8e8aa4525c2b12
Lucio Murature <luciomurature@gmail.com>
2019-09-02 23:02:55 -0300

    Update menu.jsx

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -16,8 +16,7 @@ class Menu extends Component {
             <div>
                 <h1 className="display-4">Anotador</h1>
                 <div className="alert alert-secondary" role="alert">
-                    El límite fijado temporalmente es de 100 puntos. Se pueden agregar y eliminar usuarios, el que llega a 101 puntos es automáticamente eliminado. Para empezar a anotar tocá el boton de abajo.
-                    Se recomienda organizar a cada jugador con un número para identificar unívocamente a cada uno.
+                    Anotador creado con la libreria React.js creada por Facebook.
 </div>
           <button onClick={this.start} className="btn btn-dark m-2">
             Let's Go!
